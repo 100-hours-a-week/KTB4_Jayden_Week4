3831d0dd42647e497cd5599c3352e05acd788c37
JIHYUNG KIM <martinjhkim7493@gmail.com>
2026-06-10 00:11:34 +0900

    feat: MethodArgumentNotValidException 추가

--- src/main/java/com/example/spring_rest_api/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/spring_rest_api/common/exception/GlobalExceptionHandler.java
@@ -1,5 +1,8 @@
 package com.example.spring_rest_api.common.exception;
 
+import com.example.spring_rest_api.common.response.ApiResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -7,7 +10,14 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(BusinessException.class)
-    public ErrorResponseDto handleBusiness(BusinessException exception) {
-        return ErrorResponseDto.of(exception.getMessage());
+    public ResponseEntity<ApiResponse<Void>> handleBusiness(BusinessException exception) {
+        return ResponseEntity.status(exception.getStatus())
+                .body(ApiResponse.of(exception.getMessage(), null));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiResponse<Void>> handleNotValid(MethodArgumentNotValidException exception) {
+        return ResponseEntity.status(exception.getStatusCode())
+                .body(ApiResponse.of(exception.getFieldError().getDefaultMessage(), null));
     }
 }
